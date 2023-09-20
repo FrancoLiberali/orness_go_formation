2e3643d993a326cc7cc91492b9750706a03a695e
Franco Liberali <franco.liberali@gmail.com>
2023-09-20 14:56:34 +0200

    exercise 3

--- main.go
+++ main.go
@@ -1,89 +1,64 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/FrancoLiberali/orness_go_formation/models"
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-// EX1.2 Declare a variable called albums that contains the following reggaeton albums (models.Album):
-// 1: 'Barrio fino' by 'Daddy yankee' with 23 songs
-// 2: 'King of Kings' by 'Don Omar' with 18 songs
-// HINT: use models.Album to create an album, as its definition is in the models package
-var albums = []models.Album{
-	{
-		ID:     1,
-		Name:   "Barrio Fino",
-		Artist: "Daddy yankee",
-		Songs:  23,
-	},
-	{
-		ID:     2,
-		Name:   "King of Kings",
-		Artist: "Don omar",
-		Songs:  18,
-	},
+// EX3.3 Write a function that returns the list of albums from db
+// or returns an error in case of error
+func getAlbums() ([]models.Album, error) {
 }
 
-// EX1.3 Write a function that takes an id and returns the album with that id
-// or returns an error in case the album is not found
+// EX3.5 Write a function that takes an id and returns the album with that id from db
+// or returns an error in case of error
 func getAlbumByID(id uint) (*models.Album, error) {
-	for _, album := range albums {
-		if album.ID == id {
-			return &album, nil
-		}
-	}
-
-	return nil, errors.New("album not found")
 }
 
-func getAlbumsHandler(w http.ResponseWriter, r *http.Request) {
+func getAlbumsHandler(c *gin.Context) {
 	// EX2.2 add handler that responds the list of albums in json format
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(albums)
+	albums, err := getAlbums()
+	// EX3.4 return the albums and a Status OK
+	// or the error and Internal Server Error in case of error
+	// HINT: use c.IndentedJSON
 }
 
-func getAlbumByIDHandler(w http.ResponseWriter, r *http.Request) {
+func getAlbumByIDHandler(c *gin.Context) {
 	// EX2.3 add handler that responds an album by the id present in the url
 	// or an error in case the album is not found
 	// HINT you will need to transform a string into a int
-	albumIDString := mux.Vars(r)["id"]
+	albumIDString := c.Param("id")
+
 	albumID, err := strconv.Atoi(albumIDString)
 	if err != nil {
-		log.Printf("Error transforming into int: %v", err)
-		http.Error(w, "can't transform into int", http.StatusBadRequest)
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	album, err := getAlbumByID(uint(albumID))
-	if err != nil {
-		http.Error(w, "album not found", http.StatusNotFound)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(album)
+	// EX3.6 return the album and a Status OK
+	// or Status Not Found in case the album was not found
+	// or Status Internal Server Error in case of another error
+	// HINT1: gorm will return gorm.ErrRecordNotFound in case a query does not find results
+	// HINT2: to compare errors you need to use errors.Is
 }
 
-func loggerMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s - %s (%s)", r.Method, r.URL.Path, r.RemoteAddr)
-		next.ServeHTTP(w, r)
-	})
-}
+var db *gorm.DB
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.Use(loggerMiddleware)
+	// EX3.2 connect to the sqlite database "db"
+	// and execute auto-migration for Album
+
+	router := gin.Default()
 	// EX2.4 add to the router the following routes:
 	// /album to get the list of albums
 	// /album/id to get an album by its id
-	router.HandleFunc("/album", getAlbumsHandler).Methods(http.MethodGet)
-	router.HandleFunc("/album/{id}", getAlbumByIDHandler).Methods(http.MethodGet)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	router.GET("/album", getAlbumsHandler)
+	router.GET("/album/:id", getAlbumByIDHandler)
+	log.Fatal(router.Run("localhost:8080"))
 }
--- models/album.go
+++ models/album.go
@@ -6,6 +6,7 @@ package models
 // Artist: artist name
 // Songs: amount of songs of the album
 // EX2.1: Add json tags to marshall this struct into json
+// EX3.1: Add gorm tags to save albums in a relational database
 type Album struct {
 	ID     uint   `json:"id"`
 	Name   string `json:"name"`
