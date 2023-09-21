223525bd06bde3e6303bb428485e5449d03b021f
Franco Liberali <franco.liberali@gmail.com>
2023-09-21 11:48:31 +0200

    exercise 4 solution

--- main_test.go
+++ main_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 	"testing"
 
+	"github.com/FrancoLiberali/orness_go_formation/models"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -35,6 +38,37 @@ func TestGetAlbums(t *testing.T) {
 	// you can do it with:
 	// 	var albumList []models.Album
 	// 	err = json.Unmarshal(body, &albumList)
+	resp, err := http.Get("http://localhost:8080/album")
+	assert.Nil(t, err)
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	body, err := io.ReadAll(resp.Body)
+	assert.Nil(t, err)
+
+	var albumList []models.Album
+
+	err = json.Unmarshal(body, &albumList)
+	assert.Nil(t, err)
+	assert.Equal(
+		t,
+		[]models.Album{
+			{
+				ID:     1,
+				Name:   "Barrio fino",
+				Artist: "Daddy yankee",
+				Songs:  23,
+			},
+			{
+				ID:     2,
+				Name:   "King of Kings",
+				Artist: "Don omar",
+				Songs:  18,
+			},
+		},
+		albumList,
+	)
 }
 
 func TestGetAlbumByIDThatExists(t *testing.T) {
@@ -48,9 +82,37 @@ func TestGetAlbumByIDThatExists(t *testing.T) {
 	// 		Songs:  23,
 	// 	}
 	// HINT: to verify this last point you will need to unmarshal the json returned by the api
+	resp, err := http.Get("http://localhost:8080/album/1")
+	assert.Nil(t, err)
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	body, err := io.ReadAll(resp.Body)
+	assert.Nil(t, err)
+
+	var album models.Album
+
+	err = json.Unmarshal(body, &album)
+	assert.Nil(t, err)
+	assert.Equal(
+		t,
+		models.Album{
+			ID:     1,
+			Name:   "Barrio fino",
+			Artist: "Daddy yankee",
+			Songs:  23,
+		},
+		album,
+	)
 }
 
 func TestGetAlbumByIDThatNotExists(t *testing.T) {
 	// EX4.3 Write a test that gets the album with id 3 and verifies
 	// 1. The status code of the response is 404 Not Found
+	resp, err := http.Get("http://localhost:8080/album/3")
+	assert.Nil(t, err)
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
 }
